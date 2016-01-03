9175472457c819258380939bec459d840e67f44c
SilentCicero <silentcicero@outlook.com>
2016-01-02 23:56:21 -0500

    New package

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'silentcicero:solc',
   summary: 'The solc package provides a compiler build plugin for the Meteor build tool.',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'http://github.com/SilentCicero/meteor-solc'
 });
 
 Package.registerBuildPlugin({
     name: "compileSol",
     use: [
-        "silentcicero:solc-compiler@0.1.5-1"
+        "silentcicero:solc-compiler@0.2.1"
     ],
     sources: [
         "plugin/handler.js",
